48e5205c3c775786041b8ad125d62e5acd542156
Nico Prananta <nicnocquee@yahoo.com>
2013-10-18 21:30:53 +0900

    fetch sharing token.

---
+++ PhotoBoxTests/SharingTokenTests.m
@@ -0,0 +1,74 @@
+//
+//  SharingTokenTests.m
+//  PhotoBox
+//
+//  Created by Nico Prananta on 10/18/13.
+//  Copyright (c) 2013 Touches. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <OCMock.h>
+#import "PhotoBoxClient.h"
+#import "ConnectionManager.h"
+
+@interface SharingTokenTests : XCTestCase
+
+@end
+
+@implementation SharingTokenTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testFetchSharingTokenSuccess
+{
+    NSString *photoId = @"111";
+    NSString *mockToken = @"photoToken";
+    NSString *path = [NSString stringWithFormat: @"/token/photo/%@/create.json", photoId];
+    
+    PhotoBoxClient *client = [PhotoBoxClient sharedClient];
+    id mockClient = [OCMockObject partialMockForObject:client];
+    [[[mockClient expect] andDo:^(NSInvocation *invocation) {
+        void(^successBlock)(AFHTTPRequestOperation *,id) = nil;
+        [invocation getArgument:&successBlock atIndex:4];
+        successBlock(nil, @{@"result": @{@"id": mockToken}});
+    } ] postPath:path parameters:nil success:[OCMArg any] failure:[OCMArg any]];
+    
+    [mockClient fetchSharingTokenForPhotoWithId:photoId completionBlock:^(NSString *token) {
+        XCTAssert([token isEqualToString:mockToken], @"Expected %@. Actual %@.", mockToken, token);
+    }];
+    
+    [mockClient verify];
+}
+
+- (void)testFetchSharingTokenFailure
+{
+    NSString *photoId = @"111";
+    NSString *mockToken = @"photoToken";
+    NSString *path = [NSString stringWithFormat: @"/token/photo/%@/create.json", photoId];
+    
+    PhotoBoxClient *client = [PhotoBoxClient sharedClient];
+    id mockClient = [OCMockObject partialMockForObject:client];
+    [[[mockClient expect] andDo:^(NSInvocation *invocation) {
+        void(^successBlock)(AFHTTPRequestOperation *,id) = nil;
+        [invocation getArgument:&successBlock atIndex:4];
+        successBlock(nil, @{@"result": @""});
+    } ] postPath:path parameters:nil success:[OCMArg any] failure:[OCMArg any]];
+    
+    [mockClient fetchSharingTokenForPhotoWithId:photoId completionBlock:^(NSString *token) {
+        XCTAssertFalse([token isEqualToString:mockToken], @"Expected %@. Actual %@.", mockToken, token);
+    }];
+    
+    [mockClient verify];
+}
+
+@end
