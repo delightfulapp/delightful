7fc9e6eb6606a847e74364e4316d249b5bdc205f
Nico Prananta <nicnocquee@yahoo.com>
2014-05-17 18:53:08 +0900

    fix update 1.0.1 alert

--- PhotoBox/AppDelegate.m
+++ PhotoBox/AppDelegate.m
@@ -64,12 +64,12 @@
     if (previousAppVersion) {
         previousAppVersion = [[previousAppVersion componentsSeparatedByString:@" "] firstObject];
         if ([previousAppVersion isEqualToString:@"1.0.0"]) {
-            [[ConnectionManager sharedManager] logout];
-            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Update Note" message:NSLocalizedString(@"There is caching improvement in this update but you need to be logged out of the app. Please login again. Sorry for the inconvenience.", nil) delegate:nil cancelButtonTitle:nil otherButtonTitles:NSLocalizedString(@"Dismiss", nil), nil];
+            [[ConnectionManager sharedManager] logoutWithShowingLoginScreen:NO];
+            
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Update v1.0.1" message:NSLocalizedString(@"There is caching improvement in this update but you need to be logged out of the app. Please login again. Sorry for the inconvenience.", nil) delegate:nil cancelButtonTitle:nil otherButtonTitles:NSLocalizedString(@"Dismiss", nil), nil];
             [alert show];
         }
     }
-    
     
     PanelsContainerViewController *rootViewController = [[PanelsContainerViewController alloc] init];
     
