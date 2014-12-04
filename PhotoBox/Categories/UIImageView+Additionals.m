b78523ded6d300024968cb2a855fe6b18494d665
Nico Prananta <nicnocquee@yahoo.com>
2014-12-04 14:44:49 +0900

    update sdwebimage deprecated methods

--- PhotoBox/Categories/UIImageView+Additionals.m
+++ PhotoBox/Categories/UIImageView+Additionals.m
@@ -23,30 +23,29 @@ static char operationKey;
 @implementation UIImageView (Additionals)
 
 - (void)npr_setImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder filter:(NPRImageFilter)filter {
-    [self cancelCurrentImageLoad];
+    [self sd_cancelCurrentImageLoad];
     
     self.image = placeholder;
     
     if (url)
     {
         __weak UIImageView *wself = self;
-        id<SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadWithURL:url options:0 progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished)
-                                             {
-                                                 if (!wself) return;
-                                                 if (filter==NPRImageFilterGaussianBlur) {
-                                                     image = [image blurredImage];
-                                                 }
-                                                 
-                                                 dispatch_main_sync_safe(^
-                                                                         {
-                                                                             if (!wself) return;
-                                                                             if (image)
-                                                                             {
-                                                                                 wself.image = image;
-                                                                                 [wself setNeedsLayout];
-                                                                             }
-                                                                         });
-                                             }];
+        id<SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadImageWithURL:url options:0 progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+            if (!wself) return;
+            if (filter==NPRImageFilterGaussianBlur) {
+                image = [image blurredImage];
+            }
+            
+            dispatch_main_sync_safe(^
+                                    {
+                                        if (!wself) return;
+                                        if (image)
+                                        {
+                                            wself.image = image;
+                                            [wself setNeedsLayout];
+                                        }
+                                    });
+        }];
         objc_setAssociatedObject(self, &operationKey, operation, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
 }
@@ -66,7 +65,7 @@ static char operationKey;
 - (void)npr_downloadImageWithURL:(NSURL *)url placeholder:(UIImage *)image completion:(void (^)(UIImage *))completion{
     UIActivityIndicatorView *act = [self npr_activityView];
     [act startAnimating];
-    [self setImageWithURL:url placeholderImage:image completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType) {
+    [self sd_setImageWithURL:url placeholderImage:image completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
         if (act) {
             [act stopAnimating];
         }
